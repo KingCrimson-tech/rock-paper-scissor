f8d4b732c672bd977945acbcb4bbb40e7514a50e
Chinmay <chimayrpatil123@gmail.com>
2024-11-24 11:30:39 +0530

    added style.css

--- script.js
+++ script.js
@@ -3,16 +3,21 @@
 const choices = ['rock', 'paper', 'scissor'];
 const container = document.querySelector(".container");
 const instruction = document.createElement("div");
+instruction.className = "instruction";
 container.appendChild(instruction);
 const score = document.createElement("div");
+score.className = "score";
 container.appendChild(score);
 
 let humanScore = 0;
 let computerScore = 0;
 let roundCount = 0;
+let gameOver = false;
 
 function playGame(choice) {
-    if (roundCount === 5) {
+    if (gameOver) return;
+
+    if (roundCount == 4) {
         if (humanScore > computerScore) {
             instruction.textContent = "Game over You WON!!";     
         }else if (computerScore > humanScore){
@@ -22,11 +27,14 @@ function playGame(choice) {
         }
 
         const newGame = document.createElement("button");
+        newGame.className = "btn-over";
         newGame.textContent = "New Game";
         newGame.addEventListener('click', () => {
             location.reload();
         })
         container.appendChild(newGame);
+        gameOver = true;
+        return;
     }
 
     const computerChoice = choices[Math.floor(Math.random() * 3)];
@@ -44,4 +52,5 @@ function playGame(choice) {
 
     score.textContent = `You : ${humanScore} Computer : ${computerScore}`;
     roundCount++;
+    console.log(roundCount);
 }
